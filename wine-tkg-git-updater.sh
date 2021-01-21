6d7be4aa9ced15ea11f703e87a8acd75734dfadf
Patrick M <65464529+p-mng@users.noreply.github.com>
2021-01-21 22:14:56 +0100

    Minor updates

--- wine-tkg-git-updater.sh
+++ wine-tkg-git-updater.sh
@@ -1,30 +1,21 @@
 #!/bin/sh
 
 # Find the latest release url and filename (just like in the other scripts)
-url="$(curl -s 'https://api.github.com/repos/Frogging-Family/wine-tkg-git/releases/latest' | grep 'browser_download_url' | grep 'staging' | cut -d \" -f 4)"
-filename="$(echo '$url' | sed 's|.*/||')"
-
-# Optionally download the package/do not directly install from remote
-# (not needed when directly installing using pacman)
-# curl -LOJ "$url"
+url="$(curl -s "https://api.github.com/repos/Frogging-Family/wine-tkg-git/releases/latest" | grep "browser_download_url" | grep "staging" | cut -d \" -f 4)"
 
 # Download the latest version and install it using pacman
 echo "--> Downloading and installing the latest wine-tkg..."
 
 if [ "$(id -u)" = "0" ]; then
-	echo "--> Installing as root..."
-	echo -e "\e[90m"
-	pacman --needed -U "$url"
-	echo -e "\e[0m"
-elif [ $(which sudo) ]; then
-	echo "--> Not running as root. Trying to elevate through sudo..."
-	echo -e "\e[90m"
-	sudo pacman --needed -U "$url"
-	echo -e "\e[0m"
+    echo "--> Installing as root..."
+    echo
+    pacman --needed -U "$url"
+    echo
+elif [ "$(which sudo)" ]; then
+    echo "--> Not running as root. Trying to elevate through sudo..."
+    echo
+    sudo pacman --needed -U "$url"
+    echo
 else
-	echo "--> Error: Please run the script with root privileges."
+    echo "--> Error: Please run the script with root privileges."
 fi
-
-# Cleanup (not needed when directly installing using pacman)
-# echo "--> Cleaning up..."
-# rm "$filename"
